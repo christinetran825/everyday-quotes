3fecf2dbd677cb17e04dd2b8ddd6294e1c95cb0f
github username <github email address>
2018-09-19 19:35:59 -0700

    added connect, mapped states to props, and imported the getQuote action; rendered the quote quote and author

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,39 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getQuote } from '../actions/quoteActions';
 
 class QuoteBox extends Component {
-  constructor(prop){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
-      quote: [],
-      author: []
+      quote: ""
     };
-    // this.handleChangeQuote = this.handleChangeQuote.bind(this);
+    this.handleChangeQuote = this.handleChangeQuote.bind(this);
   }
 
-  // handleChangeQuote(e){
-  //   this.setState(){
-  //     quote:
-  //     author:
-  //   }
-  // }
+  handleChangeQuote(e){
+    this.props.getQuote(this.state.quote)
+  }
 
   render() {
     return (
       <div className="wrapper">
         <div id="quote-box">
+          <h1>hello</h1>
           <div id="text">
-            {/* quote text goes here */}
-            <h1>hello</h1>
+            <h3>{this.props.quote.quote}</h3>
           </div>
           <div id="author">
-            {/* quote text goes here */}
+            <h2>{this.props.quote.author}</h2>
           </div>
-          <button id="new-quote" onClick={this.handleChangeQuote}></button>
+          <button id="new-quote" onClick={this.handleChangeQuote}>New Quote</button>
           <button><a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a></button>
+          <p></p>
         </div>
       </div>
     );
   }
 }
 
-export default QuoteBox;
+const mapStatesToProps = (state) => {
+  return ({
+    quote: state.quote,
+  });
+};
+
+export default connect(mapStatesToProps, { getQuote })(QuoteBox);
