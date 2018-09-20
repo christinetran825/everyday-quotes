a676b8161bfec6482245868fad8e51a4e0a48656
github username <github email address>
2018-09-20 09:53:25 -0700

    cleaned up file

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -6,31 +6,37 @@ class QuoteBox extends Component {
   constructor(props){
     super(props);
     this.state = {
-      quote: ""
+      quote: "",
+      clicked: false
     };
     this.handleChangeQuote = this.handleChangeQuote.bind(this);
   }
 
   handleChangeQuote(e){
     this.props.getQuote(this.state.quote)
+    this.setState({
+      clicked: true
+    })
+  }
+
+  componentDidMount() {
+    this.props.getQuote(this.state.quote)
   }
 
   render() {
     return (
-      <div className="wrapper">
-        <h1 className="page-title">Find a Quote to Inspire You Today</h1>
-        <div id="quote-box">
-          <div id="text">
-            <h3>{this.props.quote.quote}</h3>
-          </div>
-          <div id="author">
-            <h3>{this.props.quote.author}</h3>
-          </div>
+      <div id="quote-box">
+        <div id="text">
+          <h3>{this.props.quote.quote}</h3>
+        </div>
+        <div id="author">
+          <h3>{this.props.quote.author}</h3>
+        </div>
+        <div className="box-buttons">
           <button id="tweet-quote"><a href="twitter.com/intent/tweet">Tweet</a></button>
           <button id="new-quote" onClick={this.handleChangeQuote}>New Quote</button>
-          <p></p>
         </div>
-        <p className="credits">Quotes Provided by <a href="https://talaikis.com/random_quotes_api/">Tadas Talaikis</a></p>
+        <p></p>
       </div>
     );
   }
