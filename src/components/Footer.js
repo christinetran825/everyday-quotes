f6a7f9cb26ea38a837075b5ed4d51114c8e688e7
github username <github email address>
2018-09-20 09:51:18 -0700

    moved footer element from quotespage to footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="credits">Quotes Provided by <a href="https://talaikis.com/random_quotes_api/">Tadas Talaikis</a></footer>
+  );
+}
+
+export default Footer;
