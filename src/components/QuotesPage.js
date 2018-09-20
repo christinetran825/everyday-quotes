b00fa9c9bfb87b4c2cc3c41f72d446076e6b47c2
github username <github email address>
2018-09-20 09:53:01 -0700

    moved all code from App component to QuotesPage; updated the state to get the box

---
+++ src/components/QuotesPage.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import QuoteBox from './QuoteBox'
+
+class QuotesPage extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      getQuoteBox:true
+    };
+    this.handleGetQuoteBox = this.handleGetQuoteBox.bind(this);
+  }
+
+  handleGetQuoteBox(e){
+    this.setState({
+      getQuoteBox: false
+    })
+  }
+
+  render() {
+    return (
+      <main className="quotes-page">
+        <img src="./images/movie-border.jpg" alt="silent-movie-border" />
+        {this.state.getQuoteBox ?
+          <div>
+            <h1 className="cover-title">Find a Quote to Inspire You Today</h1>
+            <button className="getQuotes" onClick={this.handleGetQuoteBox}>Get Quotes</button>
+          </div>
+          :
+          <div className="page">
+            <h1 className="page-title">Find a Quote to Inspire You Today</h1>
+            <QuoteBox />
+          </div>
+        }
+      </main>
+    );
+  }
+}
+
+export default QuotesPage;
